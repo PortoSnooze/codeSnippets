56f0d32c25681a5f97c107d5a1175cbad26207e6
PortoSnooze <78099035+PortoSnooze@users.noreply.github.com>
2021-02-18 15:25:49 +0000

    Create loopVcfQualStats.R

---
+++ loopVcfQualStats.R
@@ -0,0 +1,51 @@
+# tells R to take arguments from bash script 
+args=commandArgs(trailingOnly=TRUE)
+
+library("vcfR")
+
+vcfList<-read.table(args[1], header=F)
+vcfList2<-vcfList[,]
+
+df1 = NULL
+df2 = NULL
+df3 = NULL
+
+
+for (i in vcfList2)
+{
+rm(vcf)
+rm(chrom)
+vcf <- read.vcfR(i, verbose = FALSE)
+chrom <- create.chromR(name='chromosome', vcf=vcf)
+
+# get descriptive stats for MQ
+meanMQ<-round(mean(chrom@var.info$MQ),0)
+sdMQ<-round(sd(chrom@var.info$MQ),0)
+minMQ<-round(min(chrom@var.info$MQ),0)
+maxMQ<-round(max(chrom@var.info$MQ),0)
+chromNum<-chrom@vcf@fix[1,1]
+# append results to data frame 
+df1<-rbind(df1, data.frame(chromNum,meanMQ,sdMQ,minMQ,maxMQ))
+
+# get descriptive stats for MQ
+meanDP<-round(mean(chrom@var.info$DP),0)
+sdDP<-round(sd(chrom@var.info$DP),0)
+minDP<-round(min(chrom@var.info$DP),0)
+maxDP<-round(max(chrom@var.info$DP),0)
+df2 <- rbind(df2, data.frame(meanDP,sdDP,minDP,maxDP))
+
+# get descriptive stats for QUAL
+quality<-as.numeric(chrom@vcf@fix[,6])
+meanQUAL<-round(mean(quality),0)
+sdQUAL<-round(sd(quality),0)
+minQUAL<-round(min(quality),0)
+maxQUAL<-round(max(quality),0)
+df3<-rbind(df3, data.frame(meanQUAL,sdQUAL,minQUAL,maxQUAL))
+}
+
+mergeAll<-cbind(df1,df2,df3)
+
+
+# Run in Linux using command 
+# Rscript sysTime.R $input $output
+# set a lot of memory! 
